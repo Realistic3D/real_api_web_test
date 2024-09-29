629ea96e4047a569b8ed677d14c9dd2707eedde0
HENRY亨利 <henry@thinkerx.com>
2024-09-29 18:02:14 +0800

    fix: real_api_bbl 1.0.47

--- src/render_tools/bbl_tools/BblRender.js
+++ src/render_tools/bbl_tools/BblRender.js
@@ -28,7 +28,8 @@ export default class BblRender {
         this.camera = camera;
     }
     cube() {
-        const cube = BABYLON.MeshBuilder.CreateBox("Cube", { size: 1 }, this.scene.scene);
+        // const cube = BABYLON.MeshBuilder.CreateBox("Cube", { size: 1 }, this.scene.scene);
+        const cube = BABYLON.MeshBuilder.CreateBox("Cube", { width: 5, height:0.25, depth: 5 }, this.scene.scene);
         cube.position.set(0, 0, 0);
     }
     sunLight() {
@@ -43,12 +44,18 @@ export default class BblRender {
         light.position.set(1, 1.5, 0);
     }
     areaLight() {
-        const light = new REAL.AreaLight(this.scene.scene);
-        light.position.set(0, 1, 1);
-        // light.position.set(0,0,1);
-        light.intensity = 10;
-        light.rotation.x = -Math.PI / 4;
-        // light.rotationQuaternion = new BABYLON.Quaternion.FromEulerAngles(-Math.PI / 4,0,0);
+        // const light = new REAL.AreaLight(this.scene.scene, {intensity: 10, diffuse: new BABYLON.Color3(0,1,1)});
+        // light.position.set(0, 1, -1);
+        // light.rotation.x = -Math.PI / 2;
+        const light2 = new REAL.AreaLight(this.scene.scene, {intensity: 10});
+        light2.position.set(0, 0.5, 2);
+        // light2.rotationQuaternion = new BABYLON.Quaternion.FromEulerAngles(-Math.PI / 4,-Math.PI / 4,0);
+        // light2.rotation.x = -Math.PI / 4;
+        // light2.rotation.y = -Math.PI / 4;
+        const angles = new BABYLON.Vector3(-Math.PI / 4, -Math.PI / 4, 0);
+        light2.rotationQuaternion = new BABYLON.Quaternion.FromEulerAngles(angles.x, angles.y, angles.z);
+        console.log(light2.transform().rotation)
+        console.log(light2.transform().quaternion)
     }
     pointLight() {
         const light = new REAL.PointLight(this.scene.scene);
